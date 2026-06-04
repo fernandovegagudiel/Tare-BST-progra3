7c77c00fbea8dd26516ffa84c9f5476ca50d3876
Fernando <fvegag1@miumg.edu.gt>
2026-06-03 22:12:47 -0600

    subiendo solucion de ejercicio extra 1.1

--- arboles/src/main/java/umg/edu/progra/arboles/Principal.java
+++ arboles/src/main/java/umg/edu/progra/arboles/Principal.java
@@ -13,6 +13,26 @@ package umg.edu.progra.arboles;
 public class Principal {
 
     public static void main(String[] args) {
+    	//Extra
+    	if (args.length > 0) {
+
+    	    ArbolBinarioBusqueda arbolDesdeArgs = new ArbolBinarioBusqueda();
+
+    	    for (int i = 0; i < args.length; i++) {
+    	        int valor = Integer.parseInt(args[i]);
+    	        arbolDesdeArgs.insertar(valor);
+    	    }
+
+    	    System.out.println("===== EXTRA E4: BST construido desde argumentos =====");
+
+    	    System.out.print("Valores ordenados con InOrden: ");
+    	    arbolDesdeArgs.inOrden();
+
+    	    System.out.println("Representacion visual del arbol:");
+    	    arbolDesdeArgs.imprimirArbol();
+
+    	    return;
+    	}
 
         ArbolBinarioBusqueda arbol = new ArbolBinarioBusqueda();
 
